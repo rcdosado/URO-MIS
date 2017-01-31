d995cf10940a74224e5603b9da1baf48a921dd7a
roy cyril dosado <rcdosado@gmail.com>
2017-01-31 08:35:14 +0800

    Add logout feature

--- users/views.py
+++ users/views.py
@@ -1,6 +1,21 @@
+from django.core.urlresolvers import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import logout
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views.generic import FormView
+from . import forms
 
 
 # Create your views here.
 def dashboard(request):
     return render(request, 'users/dashboard.html')
+
+
+class LogoutView(LoginRequiredMixin, FormView):
+    form_class = forms.LogoutForm
+    template_name = 'users/logout.html'
+
+    def form_valid(self, form):
+        logout(self.request)
+        return HttpResponseRedirect(reverse('home'))

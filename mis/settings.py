dc0e01a0387eefc165f1adc686f0aa324c7a87d8
roy cyril dosado <rcdosado@gmail.com>
2017-01-31 08:01:03 +0800

    Add login page

--- mis/settings.py
+++ mis/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'), ]
+
+# Authentication
+LOGIN_URL = 'users:login'
+
+LOGIN_REDIRECT_URL = 'users:dashboard'
